06e2a5ff47ec9bba9adc4cef1aa7ac71fc46024b
Gu1lh3rm3r0ch4 <128197256+Gu1lh3rm3r0ch4@users.noreply.github.com>
2023-03-24 21:37:08 -0300

    Criação do arquivo app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// Definindo uma lista
+let = [2,4,6,8,10]
+
+// Percorrendo a lista
+for(let i = 0; < numeros.length; i++){
+    if(numeros[i] % 2 === 0){
+      console.log(numeros[i] + " é um numero par")
+    }else{
+      console.log(numero[i] + " é um numero ímpar")
+    }
+  }
